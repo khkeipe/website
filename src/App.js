6d1f860e19524a4b2ea4d737756d454bb24862dd
khkeipe <kkeipe@gmail.com>
2020-08-11 16:28:04 -0500

    adding image to background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
+import sunset from './images/sunset.jpg';
 import './App.css';
 
 function App() {
   return (
-    <>
-    </>
+    <div className="App" style={{backgroundImage: `url(${sunset})` }}>
+        <Typography>Welcome</Typography>
+    </div>
   );
 }
 
