56163fcc094267cffddc07e78283b535fa1a87af
khkeipe <kkeipe@gmail.com>
2020-08-12 16:45:32 -0500

    adding custom theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, createMuiTheme, ThemeProvider } from '@material-ui/core';
 import sunset from './images/sunset.jpg';
 import './App.css';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#7f848c',
+      main: '#5f6670',
+      dark: '#42474e',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
+  typography: {
+    fontFamily: [
+      'brushland',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    fontSize: 36,
+  },
+});
 function App() {
   return (
     <div className="App" style={{backgroundImage: `url(${sunset})` }}>
-        <Typography variant="h1" color="white"> Korey Keipe</Typography>
+      <ThemeProvider theme={theme}>
+        <Typography varient="h1" color="primary"> Korey Keipe</Typography>
+      </ThemeProvider>
     </div>
   );
 }
